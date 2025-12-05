756c21e0ce735a93de1e50022090cd3955d89b24
osbrk <135859764+osbrk@users.noreply.github.com>
2025-12-05 12:35:50 +0100

    Edited test

--- OS_gnl.c
+++ OS_gnl.c
@@ -6,7 +6,7 @@
 /*   By: osukhore <osukhore@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/01 09:46:01 by osukhore          #+#    #+#             */
-/*   Updated: 2025/12/05 11:41:46 by osukhore         ###   ########.fr       */
+/*   Updated: 2025/12/05 12:34:47 by osukhore         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,31 @@
 #include <fcntl.h>
 #include <stdlib.h>
 #include "get_next_line.h"
+
+static int	ft_atoi(const char *nptr)
+{
+	int			i;
+	int			sign;
+	long long	result;
+
+	i = 0;
+	sign = 1;
+	result = 0;
+	while ((nptr[i] >= 9 && nptr[i] <= 13) || nptr[i] == 32)
+		i++;
+	if (nptr[i] == 43 || nptr[i] == 45)
+	{
+		if (nptr[i] == 45)
+			sign = -1;
+		i++;
+	}
+	while (nptr[i] >= 48 && nptr[i] <= 57)
+	{
+		result = result * 10 + (nptr[i] - '0');
+		i++;
+	}
+	return ((int)(result * sign));
+}
 
 static void	style(char style)
 {
@@ -30,16 +55,12 @@ static void	style(char style)
 int	main(int argc, char **argv)
 {
 	int		fd;
-	char	*linetest1;
-	char	*linetest2;
-	char	*linetest3;
+	char	*linetest;
 	int		i;
-	int		lines_to_print;
-
-	lines_to_print = 10;
+	int		argv_n;
 
 	// ERROR CHECK
-	if (argc < 2)
+	if (argc < 3)
 	{
 		printf("\n--------------------------------------------\n");
 		style('E');
@@ -47,53 +68,24 @@ int	main(int argc, char **argv)
 		style('R');
 		style('T');
 		printf(" Need ARGV to work with: \n\n"
-				" 1. File path \n");
+				" 1. File path \n"
+				" 2. Lines to read \n");
 		style('R');
 		printf("--------------------------------------------\n");
 		return (0);
 	}
 
+	argv_n = ft_atoi(argv[2]);
+
 	// FILE NAME
-	printf("\n--------------------------------------------\n");
 	style('T');
 	printf(" FILE: %s ", argv[1]);
 	style('R');
-	printf("\n--------------------------------------------\n");
 
-	// TEST 01: READ FIRST 1 LINE
-	style('T');
-	printf(" TEST 01: FIRST 1 LINE \n");
-	style('R');
-	printf("\n");
-
-	fd = open(argv[1], O_RDONLY);
-	if (fd < 0)
-	{
-		style('E');
-		printf("Could not open file\n");
-		style('R');
-		return (0);
-	}
-
-	linetest1 = get_next_line(fd);
-	if (linetest1 == NULL)
-	{
-		style('B');
-		printf("[EOF]\n");
-		style('R');
-	}
-	else
-	{
-		printf("1: %s", linetest1);
-		free(linetest1);
-	}
-
-	close(fd);
-
-	// TEST 02: READ FIRST 10 LINES
+	// TEST: READ FIRST N LINES
 	printf("\n--------------------------------------------\n");
 	style('T');
-	printf(" TEST 02: FIRST %d LINES \n", lines_to_print);
+	printf(" TEST: FIRST %d LINES \n", argv_n);
 	style('R');
 	printf("\n");
 
@@ -107,53 +99,16 @@ int	main(int argc, char **argv)
 	}
 
 	i = 0;
-	while (i < lines_to_print)
+	while (i < argv_n)
 	{
-		linetest2 = get_next_line(fd);
-		if (linetest2 == NULL)
-		{
-			style('B');
-			printf("[EOF]\n");
-			style('R');
-			break ;
-		}
-		printf("%d: %s", (i + 1), linetest2);
-		free(linetest2);
+		linetest = get_next_line(fd);
+		if (!linetest)
+			printf("\n");
+		printf("%d: %s", (i + 1), linetest);
+		free(linetest);
 		i++;
 	}
-
-	close(fd);
-
-
-	// TEST 03: FULL FILE READ
-	printf("\n--------------------------------------------\n");
-	style('T');
-	printf(" TEST 03: FULL FILE UNTIL EOF \n");
-	style('R');
-	printf("\n");
-
-	fd = open(argv[1], O_RDONLY);
-	if (fd < 0)
-	{
-		style('E');
-		printf("Could not open file\n");
-		style('R');
-		return (0);
-	}
-	i = 0;
-	while (1)
-	{
-		i++;
-		linetest3 = get_next_line(fd);
-		if (linetest3 == NULL)
-			break ;
-		printf("%d: %s", i, linetest3);
-		free(linetest3);
-	}
-
-	style('B');
-	printf("[EOF]\n");
-	style('R');
+	printf("\n\nRead %d lines\n\n", i);
 
 	close(fd);
 
